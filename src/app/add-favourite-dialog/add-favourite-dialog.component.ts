21f1415cd69bb0706e14b6635f725b63c443d0ee
LathikaRangappa <Lathika.Rangappa@itcinfotech.com>
2020-08-17 17:14:22 +0530

    new changes

--- src/app/add-favourite-dialog/add-favourite-dialog.component.ts
+++ src/app/add-favourite-dialog/add-favourite-dialog.component.ts
@@ -30,7 +30,11 @@ export class AddFavouriteDialogComponent implements OnInit {
     })
   }
   addToFavourities(listName, listDesc) {
+    if(listName != ''){
       this.store.dispatch(new Cart.AddProduct({ name: listName, desc: this.listDesc, value: this.data }))
+    }else{
+      alert("Input fields cannot be empty")
+    }
   }
   addNewList() {
     this.showInput = true
