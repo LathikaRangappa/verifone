0de09fcc736920a0297a4282c9c3c0f1670f721e
LathikaRangappa <Lathika.Rangappa@itcinfotech.com>
2020-08-17 16:22:54 +0530

    new changes

--- src/app/shared/service/data.service.ts
+++ src/app/shared/service/data.service.ts
@@ -6,7 +6,7 @@ export class DataService {
   constructor(private http: HttpClient) { }
 
   getSearchResult(data: string) {
-    return this.http.get("https://pi.unsplash.com/search/photos?query=" + data + "&client_id=sbBsGrbZ5dCT1jmiVOXRh5aulWC20_qfWM-s2L5mJ-U");
+    return this.http.get("https://api.unsplash.com/search/photos?query=" + data + "&client_id=sbBsGrbZ5dCT1jmiVOXRh5aulWC20_qfWM-s2L5mJ-U");
   }
 }
 
